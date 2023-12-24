d497ce115ebaf2fb3b8cd9d91503f3a7d5817ff3
ismartNishant <nishantrajputall@gmail.com>
2023-12-24 15:37:46 +0530

    all done

--- 09_mongodb/routes/Routes.js
+++ 09_mongodb/routes/Routes.js
@@ -1,56 +1,16 @@
 const express = require("express");
 const router = express.Router();
-const Blog = require("../models/blog");
+const BlogContorller = require("../controllers/BlogController");
 
 //blog routes
-router.get("/create", (req, res) => {
-    res.render("create", { title: "Create New" });
-});
+router.get("/create", BlogContorller.blog_create_get);
 
-router.get("/", (req, res) => {
-    Blog.find()
-        .sort({ createdAt: -1 })
-        .then((result) => {
-            res.render("index", { title: "All Blogs", blogs: result });
-        })
-        .catch((err) => {
-            console.log(err);
-        });
-});
+router.get("/", BlogContorller.blog_index);
 
-router.post("/", (req, res) => {
-    const blog = new Blog(req.body);
-    blog
-        .save()
-        .then((result) => {
-            res.redirect("/blogs");
-        })
-        .catch((err) => {
-            console.log(err);
-        });
-});
+router.post("/", BlogContorller.blog_create_post);
 
-router.get("/:id", (req, res) => {
-    const id = req.params.id;
-    console.log(id);
-    Blog.findById(id)
-        .then((result) => {
-            res.render("details", { title: "Blog Details", blog: result });
-        })
-        .catch((err) => {
-            console.log(err);
-        });
-});
+router.get("/:id", BlogContorller.blog_details);
 
-router.delete("/:id", (req, res) => {
-    const id = req.params.id;
-    Blog.findByIdAndDelete(id)
-        .then((result) => {
-            res.json({ redirect: "/blogs" });
-        })
-        .catch((err) => {
-            console.log(err);
-        });
-});
+router.delete("/:id", BlogContorller.blog_delete);
 
 module.exports = router;
